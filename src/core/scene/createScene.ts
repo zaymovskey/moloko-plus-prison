049f0b445fa81150014d8d89ee7a573887e5a0a5
Sergei Zaimovskii <zaims@mail.broward.edu>
2026-04-08 22:29:05 +0300

    🚀 feat(scene,almanac): Настроена сцена, свет и собрана модель альманаха

--- src/core/scene/createScene.ts
+++ src/core/scene/createScene.ts
@@ -1,1 +1,65 @@
-export function createScene(): void {}
+import * as THREE from "three";
+import "../../styles/style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { createClosedAlmanac } from "../../features/almanac/createClosedAlmanac";
+
+export function createScene(): void {
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75, // угол обзора (FOV)
+    window.innerWidth / window.innerHeight, // соотношение сторон
+    0.1, // ближняя граница
+    100, // дальняя граница
+  );
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+
+  document.body.appendChild(renderer.domElement);
+
+  camera.position.set(0, 2, 7);
+
+  scene.background = new THREE.Color(0x222222);
+
+  // Cube
+  //   const geometry = new THREE.BoxGeometry(1, 1, 1);
+  //   const material = new THREE.MeshStandardMaterial({
+  //     color: 0x00ff00,
+  //   });
+  //   const cube = new THREE.Mesh(geometry, material);
+  //   scene.add(cube);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+  scene.add(ambientLight);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(2, 2, 5);
+  scene.add(directionalLight);
+
+  const lightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+    1,
+    0xff0000,
+  );
+
+  // ------
+  scene.add(lightHelper);
+
+  const almanac = createClosedAlmanac();
+  scene.add(almanac);
+
+  function animate() {
+    controls.update();
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
--- src/features/almanac/createClosedAlmanac.ts
+++ src/features/almanac/createClosedAlmanac.ts
@@ -1,1 +1,63 @@
-export function createClosedAlmanac() {}
+import * as THREE from "three";
+
+const PAGES_COLOR = "#ffffff";
+const COVER_COLOR = "#000000";
+
+const PAGES_DEPTH = 0.3;
+const PAGES_HEIGHT = 3;
+const PAGES_WIDTH = 2;
+
+const COVER_DEPTH = 0.05;
+const COVER_HEIGHT = 3.1;
+const COVER_WIDTH = 2.1;
+
+const SPINE_WIDTH = 0.05;
+
+export function createClosedAlmanac(): THREE.Group {
+  const almanacGroup = new THREE.Group();
+
+  const pages = new THREE.BoxGeometry(PAGES_WIDTH, PAGES_HEIGHT, PAGES_DEPTH);
+  const pagesMaterial = new THREE.MeshStandardMaterial({
+    color: PAGES_COLOR,
+  });
+  const pagesMesh = new THREE.Mesh(pages, pagesMaterial);
+  almanacGroup.add(pagesMesh);
+
+  const backCover = new THREE.BoxGeometry(
+    COVER_WIDTH,
+    COVER_HEIGHT,
+    COVER_DEPTH,
+  );
+  const backCoverMaterial = new THREE.MeshStandardMaterial({
+    color: COVER_COLOR,
+  });
+  const backCoverMesh = new THREE.Mesh(backCover, backCoverMaterial);
+  backCoverMesh.position.set(0, 0, -PAGES_DEPTH / 2 - COVER_DEPTH / 2);
+  almanacGroup.add(backCoverMesh);
+
+  const frontCover = new THREE.BoxGeometry(
+    COVER_WIDTH,
+    COVER_HEIGHT,
+    COVER_DEPTH,
+  );
+  const frontCoverMaterial = new THREE.MeshStandardMaterial({
+    color: COVER_COLOR,
+  });
+  const frontCoverMesh = new THREE.Mesh(frontCover, frontCoverMaterial);
+  frontCoverMesh.position.set(0, 0, PAGES_DEPTH / 2 + COVER_DEPTH / 2);
+  almanacGroup.add(frontCoverMesh);
+
+  const spine = new THREE.BoxGeometry(
+    SPINE_WIDTH,
+    COVER_HEIGHT,
+    COVER_DEPTH * 2 + PAGES_DEPTH,
+  );
+  const spineMaterial = new THREE.MeshStandardMaterial({
+    color: COVER_COLOR,
+  });
+  const spineMesh = new THREE.Mesh(spine, spineMaterial);
+  spineMesh.position.set(-PAGES_WIDTH / 2 - SPINE_WIDTH / 2, 0, 0);
+  almanacGroup.add(spineMesh);
+
+  return almanacGroup;
+}
